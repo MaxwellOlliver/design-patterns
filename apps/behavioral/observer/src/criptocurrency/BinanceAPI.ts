1eb370f67e033ed1586a00b236962826976da517
Maxwell Macedo <maxwellmacedo2015@gmail.com>
2025-06-24 00:30:45 -0300

    Add logging to BinanceAPI for last price retrieval

--- apps/behavioral/observer/src/criptocurrency/BinanceAPI.ts
+++ apps/behavioral/observer/src/criptocurrency/BinanceAPI.ts
@@ -1,7 +1,6 @@
-import { Bitcoin } from "./Bitcoin";
-
 export class BinanceAPI {
   public getLastPrice(): number {
+    console.log("Getting last price from Binance API");
     return Math.min(Math.max(Math.random() * 1000, 0), 1000);
   }
 }
