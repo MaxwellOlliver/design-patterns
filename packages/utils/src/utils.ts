22d98fc9ded2cafd0779aea5de6ff75af698ff30
Maxwell Macedo <maxwellmacedo2015@gmail.com>
2025-07-08 03:54:00 -0300

    chore: update '@types/node' to version 20.19.4 in pnpm-lock.yaml and add utility functions for matching cases and formatting currency in utils.ts

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -1,3 +1,30 @@
 export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function match(
+  input: string,
+  cases: Record<string, () => any> & { default: () => any }
+) {
+  const value = cases[input];
+
+  if (!value) return cases.default();
+
+  return value();
+}
+
+export function formatCurrency(
+  amount: number,
+  internalPrecision: number = 2,
+  displayDecimals: number = 2,
+  locale: string = "pt-BR",
+  currency: string = "BRL"
+) {
+  const displayValue = amount / Math.pow(10, internalPrecision);
+  return displayValue.toLocaleString(locale, {
+    style: "currency",
+    currency,
+    minimumFractionDigits: displayDecimals,
+    maximumFractionDigits: displayDecimals,
+  });
+}
